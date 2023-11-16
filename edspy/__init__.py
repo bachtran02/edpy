6d4e7f03c9440768c542b8ea3762cec44c558548
Peter Tran <xuanbachtran02@gmail.com>
2023-11-15 19:57:58 -0800

    add listener and import all objects

--- edspy/__init__.py
+++ edspy/__init__.py
@@ -1,8 +1,15 @@
 import sys
 import logging
+from dotenv import load_dotenv
+
+from .client import EdClient
+from .events import *
+from .models import *
+
+load_dotenv()
 
 # config logger
-log = logging.getLogger(__name__)
+log = logging.getLogger('edspy')
 
 fmt = logging.Formatter(
     '[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s',
@@ -12,3 +19,10 @@ handler = logging.StreamHandler(sys.stdout)
 handler.setFormatter(fmt)
 log.addHandler(handler)
 log.setLevel(logging.DEBUG)
+
+
+def listener(*events: Event):
+    def wrapper(func):
+        setattr(func, '_ed_events', events)
+        return func
+    return wrapper
