78fe615c43604b0bb5f8c6c70d8212fe031e80db
bachtran <xuanbachtran02@gmail.com>
2024-01-01 07:41:32 +0700

    no need for module logger

--- edspy/__init__.py
+++ edspy/__init__.py
@@ -5,36 +5,6 @@ from .client import EdClient
 from .events import *
 from .models import *
 
-EDSPY_DEFAULT_LOGGER = {
-    'version': 1,
-    'formatters': { 
-        'default': { 
-            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s',
-            'datefmt': '%Y-%m-%d %H:%M:%S',
-        },
-    },
-    'handlers': { 
-        'default': { 
-            'formatter': 'default',
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://sys.stdout',
-        },
-    },
-    'loggers': { 
-        'edspy.transport': {
-            'handlers': ['default'],
-            'level': 'INFO',
-        }
-    }
-}
-
-def enable_logger(config: dict = EDSPY_DEFAULT_LOGGER):
-    try:
-        logging.config.dictConfig(config)
-    except Exception:
-        pass
-
-
 def listener(*events: Event):
     def wrapper(func):
         setattr(func, '_ed_events', events)
